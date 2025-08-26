2ff7f1c0bf73134e25122e7d27b2b70d9b0e00ea
mobin7869 <mobingandhi2023@gmail.com>
2025-08-26 17:29:40 +0530

    Add files via upload

---
+++ mobin 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int rows=6;
+int cols=6;
+for(int i=1;i<=rows;i++)
+{
+for(int j=1;
+ j<=cols;j++)
+{
+printf("(%d, %d)\t",i,j);
+}
+printf("\n\n");
+}
+return 0;}
+
